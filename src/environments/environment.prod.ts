e7a0092c95da514d896d7475799bcff38d12c383
YBPrasad <ybuddhikaprasad@gmail.com>
2021-10-10 00:58:18 +0530

    add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyCXzv3frsOMuid1V5c4FiXffusD0KEMCMA",
+    authDomain: "kelumengineering.firebaseapp.com",
+    databaseURL: "https://kelumengineering-default-rtdb.firebaseio.com",
+    projectId: "kelumengineering",
+    storageBucket: "kelumengineering.appspot.com",
+    messagingSenderId: "594606646160",
+    appId: "1:594606646160:web:068d7ebaec0db11bebc915",
+    measurementId: "G-S4YY2SG9TX"
+  }
 };
